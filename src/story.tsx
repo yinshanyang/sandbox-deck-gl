3ea80417ef9b15ef38c5ebcb1ce19efa7adf1987
Yin Shanyang <yang@swarm.is>
2018-06-18 19:39:32 +0800

    count overlay

--- src/story.tsx
+++ src/story.tsx
@@ -10,13 +10,10 @@ const { default: Controller } = require('./Controller')
 type Datum = {
   x: number,
   y: number,
+  z: number,
   text: string
 }
 type Domain = [number, number]
-type Domains = {
-  x: Domain,
-  y: Domain
-}
 
 const width = window.innerWidth
 const height = window.innerHeight
@@ -37,8 +34,14 @@ const view = new OrbitView()
 
 class Component extends PureComponent {
   state = {
-    text: 'text',
-    data: []
+    text: {
+      base: 'dot',
+      count: 'dot'
+    },
+    data: {
+      base: [],
+      count: []
+    }
   }
 
   componentDidMount () {
@@ -49,64 +52,111 @@ class Component extends PureComponent {
     window.removeEventListener('keydown', this.handleKeyDown)
   }
 
-  getScales = (data: Datum[]) => {
-    const domains = data
-      .reduce((memo: Domains, d: Datum) => ({
-        x: [
-          Math.min(memo.x[0], d.x),
-          Math.max(memo.x[1], d.x)
-        ],
-        y: [
-          Math.min(memo.y[0], d.y),
-          Math.max(memo.y[1], d.y)
-        ]
-      }), {
-        x: [Infinity, -Infinity],
-        y: [Infinity, -Infinity]
-      })
+  getScales = () => {
+    const { data } = this.state
+    const { base, count } = data
+
+    const xDomain = base.reduce((memo: Domain, d: Datum) => [
+      Math.min(memo[0], d.x),
+      Math.max(memo[1], d.x)
+    ], [Infinity, -Infinity])
+    const yDomain = base.reduce((memo: Domain, d: Datum) => [
+      Math.min(memo[0], d.y),
+      Math.max(memo[1], d.y)
+    ], [Infinity, -Infinity])
+    const zDomain = count.reduce((memo: Domain, d: Datum) => [
+      Math.min(memo[0], d.z),
+      Math.max(memo[1], d.z)
+    ], [Infinity, -Infinity])
 
     const scales = {
-      x: d3.scaleLinear().domain(domains.x).range([-1000, 1000]),
-      y: d3.scaleLinear().domain(domains.y).range([-1000, 1000])
+      x: d3.scaleLinear().domain(xDomain).range([-1000, 1000]),
+      y: d3.scaleLinear().domain(yDomain).range([-1000, 1000]),
+      z: d3.scalePow(0.5).domain(zDomain).range([1, 72 * 3])
     }
 
     return scales
   }
 
+  getBaseLayer = () => {
+    const { text, data } = this.state
+    const scales = this.getScales()
+
+    const baseLayer = text.base === 'text'
+      ? new TextLayer({
+        id: 'baseLayer',
+        data: data.base,
+        coordinateSystem: COORDINATE_SYSTEM.IDENTITY,
+        fontFamily: 'Fira Code, Monaco, monospace',
+        getText: ({ text }: Datum) => text,
+        getPosition: ({ x, y }: Datum) => [ scales.x(x), scales.y(y) ],
+        getSize: () => 72,
+        getColor: () => [128, 128, 128, 64]
+      })
+      : new TextLayer({
+        id: 'baseLayer',
+        data: data.base,
+        coordinateSystem: COORDINATE_SYSTEM.IDENTITY,
+        fontFamily: 'Fira Code, Monaco, monospace',
+        getText: () => '.',
+        getPosition: ({ x, y }: Datum) => [ scales.x(x), scales.y(y)],
+        getSize: () => 72,
+        getColor: () => [88, 88, 88, 64]
+      })
+
+    return baseLayer
+  }
+
+  getCountLayer = () => {
+    const { text, data } = this.state
+    const scales = this.getScales()
+
+    const countLayer = text.count === 'text'
+      ? new TextLayer({
+        id: 'countLayer',
+        data: data.count,
+        coordinateSystem: COORDINATE_SYSTEM.IDENTITY,
+        fontFamily: 'Fira Code, Monaco, monospace',
+        getText: ({ text }: Datum) => text,
+        getPosition: ({ x, y }: Datum) => [ scales.x(x), scales.y(y) ],
+        getSize: ({ z }: Datum) => scales.z(z),
+        getColor: [255, 0, 0, 255]
+      })
+      : new TextLayer({
+        id: 'countLayer',
+        data: data.count,
+        coordinateSystem: COORDINATE_SYSTEM.IDENTITY,
+        fontFamily: 'Fira Code, Monaco, monospace',
+        getText: () => '.',
+        getPosition: ({ x, y }: Datum) => [ scales.x(x), scales.y(y) ],
+        getSize: ({ z }: Datum) => scales.z(z),
+        getColor: [255, 0, 0, 255]
+      })
+
+    return countLayer
+  }
+
   render () {
-    const { data, text } = this.state
-    const scales = this.getScales(data)
-
-    const getText = text === 'text'
-      ? ({ text }: Datum) => text
-      : () => '.'
-
-    const layer = new TextLayer({
-      id: 'layer',
-      data,
-      coordinateSystem: COORDINATE_SYSTEM.IDENTITY,
-      getText: getText,
-      getPosition: ({ x, y }: Datum) => [ scales.x(x), scales.y(y) ],
-      sizeScale: 6,
-      // getSize: ({ count }: Datum) => scales.count(count),
-      getSize: () => 12,
-      getColor: () => [0, 0, 0, 64]
-    })
+    console.log(this.state)
+    const layers = [
+      this.getBaseLayer(),
+      this.getCountLayer()
+    ]
 
     return (
-      <div className='w-100 h-100 f7'>
+      <div className='w-100 h-100 f7 bg-foreground-100'>
         <DeckGL
           width='100%'
           height='100%'
           initialViewState={initialViewState}
           views={[view]}
-          layers={[layer]}
+          layers={layers}
           controller={Controller}
         />
-        <button className='absolute top-1 left-1' onClick={this.handleToggle}>
-          magic
-        </button>
-        <input type='file' className='absolute top-1 right-1' onChange={this.handleChange} />
+        <div className='absolute top-1 right-1'>
+          <input type='file' className='db mb1' onChange={this.handleChange('base')} />
+          <input type='file' className='db' onChange={this.handleChange('count')} />
+        </div>
       </div>
     )
   }
@@ -114,29 +164,61 @@ class Component extends PureComponent {
   parseCsv = (csv: string) => {
     const data = d3.csvParseRows(
       csv,
-      ([ text, x, y ]: string[]) => ({
+      ([ text, x, y, z ]: string[]) => ({
         text,
         x: +x,
-        y: +y
+        y: +y,
+        z: +z
       })
     )
-    this.setState({ data })
+    return data
   }
 
-  handleChange = (evt: any) => {
+  handleChange = (key: string) => (evt: any) => {
     const file = evt.target.files[0]
     if (!file) {
       return
     }
 
     const reader = new FileReader()
-    reader.onload = () => this.parseCsv(reader.result)
+    reader.onload = () => {
+      const data = this.parseCsv(reader.result)
+      this.setState({
+        data: {
+          ...this.state.data,
+          [key]: data
+        }
+      })
+    }
     reader.readAsText(file)
   }
 
-  handleToggle = () => this.setState({ text: this.state.text === 'text' ? 'dots' : 'text', data: this.state.data.slice() })
-
-  handleKeyDown = (evt: any) => evt.keyCode === 32 && this.handleToggle()
+  handleKeyDown = (evt: any) => {
+    if (evt.keyCode === 32) {
+      if (evt.shiftKey) {
+        this.setState({
+          text: {
+            ... this.state.text,
+            base: this.state.text.base === 'text' ? 'dot' : 'text'
+          }
+        })
+      }
+      else {
+        this.setState({
+          text: {
+            ... this.state.text,
+            count: this.state.text.count === 'text' ? 'dot' : 'text'
+          }
+        })
+      }
+      this.setState({
+        data: {
+          base: this.state.data.base.slice(),
+          count: this.state.data.count.slice()
+        }
+      })
+    }
+  }
 }
 
 storiesOf('Component', module)
